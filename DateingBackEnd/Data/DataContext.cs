3e75324a7087ef5ff4160d15f48c12a1e7dd24b9
faraz bagwan <farazbagwan14@gmail.com>
2019-08-21 00:20:17 +0530

    demo

---
+++ DateingBackEnd/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DateingBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DateingBackEnd.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
